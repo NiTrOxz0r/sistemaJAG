6724baef5b8ae47724a385042c27c51285080d0d
slayerfat <slayerfat@gmail.com>
2014-11-07 21:26:10 +0000

    añadida validacion basica de javascript a formulario de usuario

---
+++ java/validacionUsuario.js
@@ -0,0 +1,36 @@
+
+//se hizo con la idea de modificar especificamente
+//al formulario de consulta de alumno
+//porque la otra funcion requeria muchas variables
+function validarUsuario(){
+	var seudonimo = document.getElementById('seudonimo').value;
+	var clave = document.getElementById("clave").value;
+	seudonimo = seudonimo.replace(/^\s+|\s+$/g, '');
+	clave = clave.replace(/^\s+|\s+$/g, '');
+	if ( seudonimo == "" ) {
+		document.getElementById("seudonimo").focus();
+		alert("campo seudonimo no puede estar vacio");
+		return false;
+	}else{
+		return true;
+	}
+	if ( seudonimo.length == 8 ) {
+		 return true;
+	}else{
+		alert("Por favor introduzca seudonimo solo con numeros a un maximo de 8, EJ: 12345678");
+		return false;
+	}
+	if ( clave == "" ) {
+		document.getElementById("clave").focus();
+		alert("campo clave no puede estar vacio");
+		return false;
+	}else{
+		return true;
+	}
+	if ( clave.length == 8 ) {
+		 return true;
+	}else{
+		alert("Por favor introduzca clave solo con numeros a un maximo de 8, EJ: 12345678");
+		return false;
+	}
+}
--- usuario/formUsuario.php
+++ usuario/formUsuario.php
@@ -1,12 +1,35 @@
 <div>
+
+	<!-- http://www.w3schools.com/tags/ref_eventattributes.asp -->
 	<form action="usuario/consultarUsuario.php" method="POST">
 
-		Usuario:<input type="text" name="seudonimo" placeholder="Instroduzca Usuario"></br>
+		Usuario:
+		<input 
+			type="text" 
+			name="seudonimo" 
+			id="seudonimo"
+			autofocus="autofocus"
+			required="required"
+			placeholder="Instroduzca Usuario">
+		</br>
 
-		Clave: <input type="password" name="clave" placeholder="Instroduzca Clave"></br>
-		<input type="submit" value="enviar" name="enviar">
+		Clave: 
+		<input 
+			type="password" 
+			name="clave" 
+			id="clave"
+			required="required"
+			placeholder="Instroduzca Clave"></br>
+		<input 
+			type="submit" 
+			value="enviar" 
+			name="enviar" 
+			onsubmit"return validarUsuario();"
+			onclick="return validarUsuario();">
 
 	</form>
 
 	<a href="reg.php">Registrarse</a>
 </div>
+
+<script type="text/javascript" src="java/validacionUsuario.js"></script>
