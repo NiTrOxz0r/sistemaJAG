033df3a6511465fec7f727156932f818134e8331
slayerfat <slayerfat@gmail.com>
2014-11-11 00:19:20 -0430

    implementacion al 70%

--- Personal_Autorizado/consultar_P.php
+++ Personal_Autorizado/consultar_P.php
@@ -128,13 +128,17 @@ empezarPagina();
 						</p>
 					</div>
 
-			<?php	else:?>
-					<span>
-						No existe(n) Alumno(s) relacionado(s) con: 
-						<?php echo $datos['p_apellido_r']; ?>,
-						<?php echo $datos['p_nombre_r']; ?>
-					</span>
-			<?php	endif;?>
+				<?php	else:?>
+						<span>
+							La cedula <strong><?php echo $cedula_r ?></strong>
+							no tiene ninguna relacion con ningun alumno!
+						</span>
+						<p>
+							<a href="#">Actualizar datos referentes a esta cedula</a>
+							</br>
+							<a href="#">ver relaciones de esta cedula</a>
+						</p>
+				<?php	endif;?>
 			<?php else: ?>
 				<span>
 					Error, cedula Incorrecta, intente nuevamente
@@ -144,16 +148,265 @@ empezarPagina();
 				</p>
 			<?php endif ?>
 			
-		<?php elseif(isset($_GET['cedula_a'] or isset($_GET['cedula_escolar_a'])): ?>
-			<?php if (isset($_GET['cedula_a']): ?>
-				<?php $cedula_a = trim($_GET['cedula_a']); ?>
-			<?php else: ?>
-				<?php $cedula_a = trim($_GET['cedula_escolar_a']); ?>
-			<?php endif ?>
+		<?php elseif( isset($_GET['cedula_a']) ) : ?>
+			<?php $cedula_a = trim($_GET['cedula_a']); ?>
 			<?php if (strlen($cedula_a) == 8):
 				$con = conexion();
-				$cedula_a = mysqli_escape_string($con, $cedula_a);?>
-				continuar...
+				$cedula_a = mysqli_escape_string($con, $cedula_a);
+
+				//buscamos los representantes que esten relacionados
+				//con este alumno
+				$query = "SELECT 
+				alumno.p_apellido as p_apellido_a,
+				alumno.p_nombre as p_nombre_a,
+				personal_autorizado.codigo as codigo_r,
+				personal_autorizado.p_apellido as p_apellido_r,
+				personal_autorizado.p_nombre as p_nombre_r,
+				personal_autorizado.relacion as relacion_r,
+				personal_autorizado.telefono as telefono_r,
+				personal_autorizado.telefono_otro as telefono_otro_r,
+				personal_autorizado.email as email_r,
+				personal_autorizado.vive_con_alumno as vive_con_alumno_r
+				from obtiene
+				inner join personal_autorizado
+				on obtiene.cod_p_a = personal_autorizado.codigo
+				inner join alumno
+				on obtiene.cod_alu = alumno.codigo
+				inner join curso
+				on alumno.cod_curso = curso.codigo
+				where alumno.cedula = $cedula_a
+				order by alumno.codigo;";
+				$resultado = conexion($query);?>
+
+				<?php if ($resultado->num_rows <> 0) :?>
+					
+					<?php $unaVez = true; ?>
+					<?php	while ($datos = mysqli_fetch_array($resultado)) : ?>
+
+						<?php if ($unaVez): ?>						
+							<span>
+								Personas relacionados con: 
+								<?php echo $datos['p_apellido_a']; ?>,
+								<?php echo $datos['p_nombre_a']; ?>
+							</span>
+						<?php endif; $unaVez = false; ?>
+
+						<table>
+							<thead>
+								<th>
+									Primer Apellido
+								</th>
+								<th>
+									Segundo Apellido
+								</th>
+								<th>
+									Primer Apellido
+								</th>
+								<th>
+									Segundo Apellido
+								</th>
+								<th>
+									Cedula
+								</th>
+								<th>
+									Cedula Escolar
+								</th>
+								<th>
+									Curso Asignado
+								</th>
+							</thead>
+							<tbody>
+								<td>
+									<?php echo $datos['p_apellido_r']; ?>
+								</td>
+								<td>
+									<?php echo $datos['p_nombre_r']; ?>
+								</td>
+								<td>
+									<?php echo $datos['relacion_r']; ?>
+								</td>
+								<td>
+									<?php echo $datos['telefono_r']; ?>
+								</td>
+								<td>
+									<?php echo $datos['telefono_otro_r']; ?>
+								</td>
+								<td>
+									<?php echo $datos['email_r']; ?>
+								</td>
+								<td>
+									<?php echo $datos['vive_con_alumno_r']; ?>
+								</td>
+								<td>
+									<a href="#">
+										<button>Editar</button>
+									</a>
+								</td>
+							</tbody>
+						</table>
+
+					<?php endwhile;?>
+
+					<div>
+						<p>
+							Agregar otro alumno relacionado con este representante.
+						</p>
+						<p>
+							Agregar un familiar o personal autorizado que pueda retirar al alumno.
+						</p>
+						<p>
+							Culminar el proceso de inscripcion (GENERACION DE REPORTE; ETC!!)
+						</p>
+					</div>
+
+				<?php	else:?>
+						<span>
+							La cedula <strong><?php echo $cedula_a ?></strong>
+							no tiene ninguna relacion con ningun representante o persona allegada!
+						</span>
+						<p>
+							<a href="#">Actualizar informacion referente a esta cedula</a>
+							</br>
+							<a href="#">ver relaciones de esta cedula</a>
+						</p>
+				<?php	endif;?>
+			<?php else: ?>
+				<span>
+						Error, cedula Incorrecta, intente nuevamente
+					</span>
+					<p>
+						<a href="#">A DONDE TIENE QUE IR...</a>
+					</p>
+			<?php endif ?>
+		<?php elseif( isset($_GET['cedula_escolar_a']) ) : ?>
+			<?php $cedula_a = trim($_GET['cedula_escolar_a']); ?>
+			<?php if (strlen($cedula_a) == 8):
+				$con = conexion();
+				$cedula_a = mysqli_escape_string($con, $cedula_a);
+
+				//buscamos los representantes que esten relacionados
+				//con este alumno
+				$query = "SELECT 
+				alumno.p_nombre as p_nombre_a,
+				alumno.p_nombre as p_nombre_a,
+				personal_autorizado.codigo as codigo_r,
+				personal_autorizado.p_apellido as p_apellido_r,
+				personal_autorizado.p_nombre as p_nombre_r,
+				personal_autorizado.relacion as relacion_r,
+				personal_autorizado.telefono as telefono_r,
+				personal_autorizado.telefono_otro as telefono_otro_r,
+				personal_autorizado.email as email_r,
+				personal_autorizado.vive_con_alumno as vive_con_alumno_r
+				from obtiene
+				inner join personal_autorizado
+				on obtiene.cod_p_a = personal_autorizado.codigo
+				inner join alumno
+				on obtiene.cod_alu = alumno.codigo
+				inner join curso
+				on alumno.cod_curso = curso.codigo
+				where alumno.cedula = $cedula_a
+				order by alumno.codigo;";
+				$resultado = conexion($query);?>
+				<?php if ($resultado->num_rows <> 0) :?>
+					
+					<?php $unaVez = true; ?>
+					<?php	while ($datos = mysqli_fetch_array($resultado)) : ?>
+
+						<?php if ($unaVez): ?>						
+							<span>
+								Personas relacionados con: 
+								<?php echo $datos['p_apellido_a']; ?>,
+								<?php echo $datos['p_nombre_a']; ?>
+							</span>
+						<?php endif; $unaVez = false; ?>
+
+						<table>
+							<thead>
+								<th>
+									Primer Apellido
+								</th>
+								<th>
+									Segundo Apellido
+								</th>
+								<th>
+									Primer Apellido
+								</th>
+								<th>
+									Segundo Apellido
+								</th>
+								<th>
+									Cedula
+								</th>
+								<th>
+									Cedula Escolar
+								</th>
+								<th>
+									Curso Asignado
+								</th>
+							</thead>
+							<tbody>
+								<td>
+									<?php echo $datos['p_apellido_r']; ?>
+								</td>
+								<td>
+									<?php echo $datos['p_nombre_r']; ?>
+								</td>
+								<td>
+									<?php echo $datos['relacion_r']; ?>
+								</td>
+								<td>
+									<?php echo $datos['telefono_r']; ?>
+								</td>
+								<td>
+									<?php echo $datos['telefono_otro_r']; ?>
+								</td>
+								<td>
+									<?php echo $datos['email_r']; ?>
+								</td>
+								<td>
+									<?php echo $datos['vive_con_alumno_r']; ?>
+								</td>
+								<td>
+									<a href="#">
+										<button>Editar</button>
+									</a>
+								</td>
+							</tbody>
+						</table>
+
+					<?php endwhile;?>
+
+					<div>
+						<p>
+							Agregar otro alumno relacionado con este representante.
+						</p>
+						<p>
+							Agregar un familiar o personal autorizado que pueda retirar al alumno.
+						</p>
+						<p>
+							Culminar el proceso de inscripcion (GENERACION DE REPORTE; ETC!!)
+						</p>
+					</div>
+
+				<?php	else:?>
+						<span>
+							La cedula <strong><?php echo $cedula_a ?></strong>
+							no tiene ninguna relacion con ningun representante o persona allegada!
+						</span>
+						<p>
+							<a href="#">Actualizar informacion referente a esta cedula</a>
+							</br>
+							<a href="#">ver relaciones de esta cedula</a>
+						</p>
+				<?php	endif;?>
+			<?php else: ?>
+				<span>
+						Error, cedula Incorrecta, intente nuevamente
+					</span>
+					<p>
+						<a href="#">A DONDE TIENE QUE IR...</a>
+					</p>
+			<?php endif ?>
 		<?php else: ?>
 			<span>
 					Error, cedula Incorrecta, intente nuevamente
