bda7b29a5cc5c84ddc4bf5172271877aff3f8210
slayerfat <slayerfat@gmail.com>
2014-11-14 18:37:00 -0430

    creando jQuery para validacion de menucon completado en 40%

--- usuario/menucon.php
+++ usuario/menucon.php
@@ -18,7 +18,11 @@ empezarPagina();
 		<!-- DETALLESE QUE NO ES UN ID SINO UNA CLASE. -->
 		<div class="contenido">
 
-			<form id="consulta_U">
+			<form 
+				id="consulta_singular_U" 
+				name="consulta_singular_U"
+				action="consultar_U.php"
+				method="post">
 
 				<table>
 					<thead>
@@ -33,28 +37,45 @@ empezarPagina();
 									id="tipo"
 									autofocus="autofocus"
 									required>
-									<option selected="selected">--Seleccione--</option>
+									<option selected="selected" value="0">--Seleccione--</option>
 									<option value="1">Por cedula:</option>
 									<option value="2">Por Nombre:</option>
 									<option value="3">Por Apellido:</option>
 									<option value="4">Por Cargo:</option>
 									<option value="5">Por Estatus:</option>
-									<option value="6">Por Curso:</option>
 								</select>
 							</td>
 							<td class="chequeo" id="tipo_chequeo">
 								
 							</td>
+						</tr>
+						<tr>
+							<td id="informacion_titulo">
+								Favor especifique:
+							</td>
 							<td>
-								<input 
+								<input
 									type="text"
 									name="informacion"
-									id="informacion"
-									required
-									disabled>
+									id="informacion">
+									<?php $query = "SELECT codigo, descripcion from cargo where status = 1;";
+										$resultado = conexion($query);?>
+								<select name="informacion" id="informacion_lista" hidden>
+									<option value="" selected="selected">--Seleccione--</option>
+									<?php while ( $datos = mysqli_fetch_array($resultado) ) : ?>
+										<option value="<?php echo $datos['codigo']; ?>">
+											<?php echo $datos['descripcion']; ?>
+										</option>
+									<?php endwhile; ?>
+								</select>
 							</td>
-							<td id="informacion_chequeo">
+							<td class="chequeo" id="informacion_chequeo">
 								
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<input type="submit" id="submit" value="Consultar">
 							</td>
 						</tr>
 					</tbody>
@@ -69,7 +90,11 @@ empezarPagina();
 
 			</form>
 
-			<form>
+			<form 
+				id="consulta_listado_U" 
+				name="consulta_listado_U"
+				action="consultar_listado_U.php"
+				method="post">
 				<table>
 					<thead>
 						<th>Consulta General</th>
@@ -90,6 +115,85 @@ empezarPagina();
 			</div>
 		
 		</div>
+		<!-- validacion -->
+		<script type="text/javascript">
+			$(function(){
+				$('#informacion_titulo').css('color', '#888');
+				$('#informacion').prop('disabled', true);
+				$('#submit').prop('disabled', true);
+				//el select:
+				$('#tipo').on('change', function(){
+					var tipo = $(this).val();
+					if (tipo === '0') {
+						$('#informacion_titulo').css('color', '#888');
+						$('#informacion').prop('disabled', true);
+						$('#submit').prop('disabled', true);
+					}else{
+						$('#informacion_titulo').css('color', '#000');
+						$('#informacion').prop('disabled', false);
+					};
+				});
+				$('#informacion').on('change', function(){
+					var campo = $(this).val().replace(/^\s+|\s+$/g, '');
+					if (campo === "") {
+						$('#submit').prop('disabled', true);
+						$(this).focus();
+						$("#informacion_chequeo").html('este campo no puede </br> estar vacio.');
+						$("#informacion_titulo").css('color', 'red');
+					};
+					
+					var tipo = $('#tipo').val();
+					var numerosChequeo = /[^\d+]/g;
+					var	nombresChequeo = /[^A-Za-záéíóúÁÉÍÓÚ-]/g;
+					if (tipo === '1') {
+
+						if(campo.length < 6){
+							$('#submit').prop('disabled', true);
+							$(this).focus();
+							$("#informacion_chequeo").html('cedula no puede ser menor a 6 caracteres');
+							$("#informacion_titulo").css('color', 'red');
+						}else if(campo.length > 8){
+							$('#submit').prop('disabled', true);
+							$(this).focus();
+							$("#informacion_chequeo").html('cedula no puede ser mayor a 8 caracteres');
+							$("#informacion_titulo").css('color', 'red');
+						}else if( numerosChequeo.exec(campo) ){
+							$('#submit').prop('disabled', true);
+							$(this).focus();
+							$("#informacion_chequeo").html('Favor introduzca cedula solo numeros sin caracteres especiales, EJ: 12345678');
+							$("#informacion_titulo").css('color', 'red');
+						}else{
+							$("#informacion_chequeo").html('');
+							$("#informacion_titulo").css('color', 'green');
+							$('#submit').prop('disabled', false);
+						}
+
+					}else if( tipo === '2' || tipo === '3' ) {
+
+						if(campo.length > 20){
+							$('#submit').prop('disabled', true);
+							$(this).focus();
+							$("#informacion_chequeo").html('este campo no puede ser mayor a 20 caracteres');
+							$("#informacion_titulo").css('color', 'red');
+						}else if( nombresChequeo.test(campo) ){
+							$('#submit').prop('disabled', true);
+							$(this).focus();
+							$("#informacion_chequeo").html('Favor introduzca en este campo Letras sin numeros o caracteres especiales EJ: 19?=;@*');
+							$("#informacion_titulo").css('color', 'red');
+						}else{
+							$("#informacion_chequeo").html('');
+							$("#informacion_titulo").css('color', 'green');
+							estatus = true;
+						}
+
+					}else if(tipo === '4' || tipo === '5') {
+						$('#submit').prop('disabled', true);
+						$(this).prop('disabled', false);
+						$('#informacion').prop('hidden', true);
+					};
+				});
+			});
+		</script>
 		<!-- CONTENIDO TERMINA ARRIBA DE ESTO: -->
 	</div>
 </div>
