3eefb4965efc0533fee73358566330cc563f74af
slayerfat <slayerfat@gmail.com>
2014-11-22 00:31:47 -0430

    eliminada la locura de @Phantom66 en jQuery.

--- Personal_Autorizado/menucon.php
+++ Personal_Autorizado/menucon.php
@@ -43,27 +43,27 @@ validarUsuario(1);?>
 				<?php $cargador = enlaceDinamico("java/ajax/cargadorOnClick.js"); ?>
 				<script type="text/javascript" src="<?php echo $cargador; ?>"></script>
 				<script type="text/javascript">
+					$(function(){
+						//cedula Representante
+						$('#form_r :submit').on('click', function(evento){
+							evento.preventDefault();
+							var campo = $('#form_r [name=cedula_r]').val().trim();
+							var cedRegex = /^[0-9]+$/;
+							if (campo != "" && campo.length == 8) {
+								if (campo.match(cedRegex)) {
+									$('#form_r').submit();
+									return true;
+								}else{
+									$('#cedula_chequeo_r').html('por favor introduzca la cedula sin espacios o caracteres especiales ej: 12345678');
+									return false
+								};
 
-					//cedula Representante
-					$('#form_r :submit').on('click', function(evento){
-						evento.preventDefault();
-						var campo = $('#form_r [name=cedula]').val().trim();
-						var cedRegex = /^[0-9]+$/;
-						if (campo != "" && campo.length == 8) {
-							if (campo.match(cedRegex)) {
-								$('#form_r').submit();
-								return true;
 							}else{
 								$('#cedula_chequeo_r').html('por favor introduzca la cedula sin espacios o caracteres especiales ej: 12345678');
-								return false
-							};
+								return false};
+						});
 
-						}else{
-							$('#cedula_chequeo_r').html('por favor introduzca la cedula sin espacios o caracteres especiales ej: 12345678');
-							return false};
 					});
-
-				});
 				</script>
 			</div>
 		</div>
