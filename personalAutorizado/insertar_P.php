58df54bbf04001e84ff16322ec9bafda7b123f7f
slayerfat <slayerfat@gmail.com>
2014-12-04 12:38:10 -0430

    terminado validacion de P en insert, falta update.

--- personalAutorizado/insertar_P.php
+++ personalAutorizado/insertar_P.php
@@ -18,13 +18,13 @@ $enlace = $_SERVER['DOCUMENT_ROOT']."/github/sistemaJAG/php/master.php";
 require_once($enlace);
 // invocamos validarUsuario.php desde master.php
 validarUsuario(1, 1, $_SESSION['cod_tipo_usr']);
-empezarPagina($_SESSION['cod_tipo_usr'], $_SESSION['cod_tipo_usr']);
+
 if (isset($_POST['cedula']) and preg_match( "/[0-9]{8}/", $_POST['cedula']) ) :
 
   $status = 1;
   $validarPA = new ChequearPA(
     $_SESSION['codUsrMod'],
-    'eñe EÑE',
+    $_POST['p_apellido'],
     $_POST['s_apellido'],
     $_POST['p_nombre'],
     $_POST['s_nombre'],
@@ -58,15 +58,24 @@ if (isset($_POST['cedula']) and preg_match( "/[0-9]{8}/", $_POST['cedula']) ) :
       telefono_otro,
       status,
       cod_usr_reg,
-      cod_usr_mod
+      cod_usr_mod,
+      fec_mod
     )
-    VALUES('$cedula_r','$nacionalidad',
-      '$p_nombre','$s_nombre',
-      '$p_apellido',
-      '$s_apellido','$sexo',
-      '$fec_nac','$telefono',
-      '$telefono_otro','$status',
-      '$cod_usr_reg','$cod_usr_mod'
+    VALUES(
+      $validarPA->cedula,
+      $validarPA->nacionalidad,
+      $validarPA->p_nombre,
+      $validarPA->s_nombre,
+      $validarPA->p_apellido,
+      $validarPA->s_apellido,
+      $validarPA->sexo,
+      $validarPA->fecNac,
+      $validarPA->telefono,
+      $validarPA->telefonoOtro,
+      $status,
+      $validarPA->codUsrMod,
+      $validarPA->codUsrMod,
+      current_timestamp
     );";
     // poner diferentes nombres de query es unutil y confuso:
     // al queryP y queryDirP se asume que hay un query y un queryDir aparte
@@ -75,47 +84,135 @@ if (isset($_POST['cedula']) and preg_match( "/[0-9]{8}/", $_POST['cedula']) ) :
     // pero en este flujo, solo se usa un query a la vez.
     // para que crear 4 variables si con una sola se hace todo?
     // eficacia y eficiencia leotur por el amor a dios.
-    $res = conexion($query,1);
+    $res = conexion($query);
 
-    $query = "SELECT codigo from persona where cedula = '$cedula_r';";
-    $resultado = conexion($query,1);
+    $query = "SELECT codigo from persona where cedula = $validarPA->cedula;";
+    $resultado = conexion($query);
     $datos = mysqli_fetch_assoc($resultado);
     $cod_persona = $datos['codigo'];
 
-    $query = "INSERT INTO direccion
-      (
-      cod_persona,
-      cod_parroquia,
-      direccion_exacta,
-      status,
-      cod_usr_reg,
-      cod_usr_mod)
-      VALUES('$cod_persona','$cod_parroquia','$direccion_exacta', '$status','$cod_usr_reg','$cod_usr_mod');";
+    $validarDireccion = new ChequearDireccion(
+      $_SESSION['codUsrMod'],
+      $cod_persona,
+      $_POST['cod_parro'],
+      $_POST['direcc']
+    );
 
-    $res = conexion($query,1);
+    if ( $validarDireccion->valido() ) :
+      $query = "INSERT INTO direccion(
+        cod_persona,
+        cod_parroquia,
+        direccion_exacta,
+        status,
+        cod_usr_reg,
+        cod_usr_mod,
+        fec_mod
+      )
+      VALUES(
+        $validarDireccion->codPersona,
+        $validarDireccion->codParroquia,
+        $validarDireccion->direccionExacta,
+        $status,
+        $validarDireccion->codUsrMod,
+        $validarDireccion->codUsrMod,
+        current_timestamp
+      );";
 
-    $query = "INSERT INTO personal_autorizado(
-    cod_persona,
-    lugar_nac,
-    email,
-    relacion,
-    vive_con_alumno,
-    nivel_instruccion,
-    profesion,
-    lugar_trabajo,
-    direccion_trabajo,
-    telefono_trabajo,
-    status,
-    cod_usr_reg,
-    cod_usr_mod
-    )
-    VALUES('$cod_persona','$lugar_nac','$email','$relacion','$vive_con_alumno', '$nivel_instruccion',
-    '$profesion','$lugar_trabajo','$direccion_trabajo','$telefono_trabajo','$status','$cod_usr_reg','$cod_usr_mod');";
+      $res = conexion($query);
 
-    $res = conexion($query,1);
+      $query = "INSERT INTO personal_autorizado(
+        cod_persona,
+        lugar_nac,
+        email,
+        relacion,
+        vive_con_alumno,
+        nivel_instruccion,
+        profesion,
+        lugar_trabajo,
+        direccion_trabajo,
+        telefono_trabajo,
+        status,
+        cod_usr_reg,
+        cod_usr_mod,
+        fec_mod
+      )
+      VALUES(
+        $validarDireccion->codPersona,
+        $validarPA->lugNac,
+        $validarPA->email,
+        $validarPA->relacion,
+        $validarPA->viveConAlumno,
+        $validarPA->nivelInstruccion,
+        $validarPA->profesion,
+        $validarPA->lugarTrabajo,
+        $validarPA->direccionTrabajo,
+        $validarPA->telefonoTrabajo,
+        $status,
+        $validarPA->codUsrMod,
+        $validarPA->codUsrMod,
+        current_timestamp
+      );";
 
-    header("Location:../alumno/form_reg_A.php?cedula_r=$cedula_r");
+      $res = conexion($query);
+
+      header("Location:../alumno/form_reg_A.php?cedula_r=$_POST[cedula]");
+    else :
+      empezarPagina($_SESSION['cod_tipo_usr'], $_SESSION['cod_tipo_usr']); ?>
+      <div id="contenido_insertar_P">
+        <div id="blancoAjax">
+          <div class="container">
+            <div class="row">
+              <div class="jumbotron">
+                <h1>Ups!</h1>
+                <p>
+                  Error en el proceso de registro!
+                </p>
+                <h3>
+                  Los datos suministrados al sistema parecen ser invalidos!
+                </h3>
+                <div class="bg-danger">
+                  <p>
+                    <em>Especificamente el sistema declara:</em>
+                  </p>
+                  <p>
+                     <strong>
+                       <em>
+                         <?php echo $validarDireccion->info() ?>
+                       </em>
+                     </strong>
+                  </p>
+                </div>
+                <p class="bg-info">
+                  Si considera que esto no es un error, contacte a un administrador del sistema.
+                </p>
+                <?php $inscripcion = enlaceDinamico('personalAutorizado/form_reg_P.php'); ?>
+                <p>
+                  para ir al proceso de inscripcion <a href="<?php echo $inscripcion ?>">
+                  puede seguir este enlace.
+                  </a>
+                </p>
+                <p>
+                  Si desea hacer una consulta por favor dele
+                  <a href="menucon.php">click a este enlace.</a>
+                </p>
+                <p>
+                  ¿O sera que entro en esta pagina erroneamente?
+                </p>
+                <p class="bg-warning">
+                  Si este es un problema recurrente, contacte a un administrador del sistema.
+                </p>
+                <p>
+                  <?php $index = enlaceDinamico(); ?>
+                  <a href="<?php echo $index ?>" class="btn btn-primary btn-lg">Regresar al sistema</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  <?php endif;
   else : ?>
+    empezarPagina($_SESSION['cod_tipo_usr'], $_SESSION['cod_tipo_usr']); ?>
     <div id="contenido_insertar_P">
       <div id="blancoAjax">
         <div class="container">
@@ -171,7 +268,8 @@ if (isset($_POST['cedula']) and preg_match( "/[0-9]{8}/", $_POST['cedula']) ) :
   <?php endif;
 
 
-else: ?>
+else:
+  empezarPagina($_SESSION['cod_tipo_usr'], $_SESSION['cod_tipo_usr']); ?>
   <div id="contenido_insertar_P">
     <div id="blancoAjax">
       <div class="container">
