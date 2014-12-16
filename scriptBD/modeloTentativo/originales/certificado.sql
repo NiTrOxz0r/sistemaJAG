9299418608d2e2f051aa5d930a214bb06961cf04
slayerfat <slayerfat@gmail.com>
2014-12-16 10:52:04 -0430

    creada tabla referencial de certificados

---
+++ scriptBD/modeloTentativo/originales/certificado.sql
@@ -0,0 +1,30 @@
+CREATE TABLE certificado (
+  codigo tinyint(3) unsigned primary key,
+  descripcion varchar(30) not null COMMENT 'a espera de mas datos',
+  status tinyint(1) unsigned not null default 1,
+  cod_usr_reg int unsigned not null,
+  fec_reg timestamp not null default current_timestamp,
+  cod_usr_mod int unsigned not null,
+  fec_mod timestamp not null DEFAULT 0,
+  foreign key (cod_usr_reg)
+    references usuario(codigo)
+    on update cascade
+    on delete restrict,
+  foreign key (cod_usr_mod)
+    references usuario(codigo)
+    on update cascade
+    on delete restrict
+)
+CHARACTER SET utf8
+COLLATE utf8_general_ci
+COMMENT = 'titulos o certificados de personal';
+
+insert into certificado values
+  (1, '-', 1, 1, current_timestamp, 1, current_timestamp),
+  (2, 'Curso', 1, 1, current_timestamp, 1, current_timestamp),
+  (3, 'Capacitacion', 1, 1, current_timestamp, 1, current_timestamp),
+  (4, 'Diplomado', 1, 1, current_timestamp, 1, current_timestamp),
+  (5, 'Especializacion', 1, 1, current_timestamp, 1, current_timestamp),
+  (6, 'Maestria', 1, 1, current_timestamp, 1, current_timestamp),
+  (7, 'Otro', 1, 1, current_timestamp, 1, current_timestamp)
+;
