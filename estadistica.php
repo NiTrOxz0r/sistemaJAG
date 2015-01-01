42a845673ea4048d774a28e6e11153cbb2cdcc00
slayerfat <slayerfat@gmail.com>
2015-01-01 17:22:12 -0430

    añadidos datos concretos.

--- estadistica.php
+++ estadistica.php
@@ -42,23 +42,38 @@ empezarPagina($_SESSION['cod_tipo_usr'], $_SESSION['cod_tipo_usr']); ?>
                 </div>
               </div>
               <!-- personas en sistema -->
+              <?php $query = "SELECT count(codigo) as total
+              from persona where status = 1;";
+              $resultado = conexion($query);
+              $datos = mysqli_fetch_assoc($resultado); ?>
               <div class="row">
                 <div class="col-sm-12">
                   <h4>
-                    Personas en el sistema: <strong>algo</strong>
+                    Personas en el sistema:
+                    <strong><?php echo $datos['total'] ?></strong>
                   </h4>
                 </div>
               </div>
               <!-- por sexo -->
+              <?php $query = "SELECT
+              SUM(CASE WHEN sexo = '0' THEN 1 ELSE 0 END) hombres,
+              SUM(CASE WHEN sexo = '1' THEN 1 ELSE 0 END) mujeres,
+              COUNT(*) total
+              FROM persona
+              where status = 1;";
+              $resultado = conexion($query);
+              $datos = mysqli_fetch_assoc($resultado); ?>
               <div class="row">
                 <div class="col-sm-6">
                   <h4>
-                    Personas de sexo Masculino: <strong>algo</strong>
+                    Personas de sexo Masculino:
+                    <strong><?php echo $datos['hombres'] ?></strong>
                   </h4>
                 </div>
                 <div class="col-sm-6">
                   <h4>
-                    Personas de sexo Femenino: <strong>algo</strong>
+                    Personas de sexo Femenino:
+                    <strong><?php echo $datos['mujeres'] ?></strong>
                   </h4>
                 </div>
               </div>
@@ -82,96 +97,191 @@ empezarPagina($_SESSION['cod_tipo_usr'], $_SESSION['cod_tipo_usr']); ?>
                 </div>
               </div>
               <!-- en sistema -->
+              <?php $query = "SELECT count(codigo) as total
+              from alumno where status = 1;";
+              $resultado = conexion($query);
+              $datos = mysqli_fetch_assoc($resultado); ?>
               <div class="row">
                 <div class="col-sm-12">
                   <h4>
-                    Alumnos en el sistema: <strong>algo</strong>
+                    Alumnos en el sistema:
+                    <strong><?php echo $datos['total'] ?></strong>
                   </h4>
                 </div>
               </div>
               <!-- por sexo -->
+              <?php $query = "SELECT
+              SUM(CASE WHEN sexo = '0' THEN 1 ELSE 0 END) ninos,
+              SUM(CASE WHEN sexo = '1' THEN 1 ELSE 0 END) ninas,
+              COUNT(*) total
+              FROM persona
+              inner join alumno
+              on persona.codigo = alumno.cod_persona
+              where alumno.status = 1;";
+              $resultado = conexion($query);
+              $datos = mysqli_fetch_assoc($resultado); ?>
               <div class="row">
                 <div class="col-sm-6">
                   <h4>
-                    Alumnos de sexo Masculino: <strong>algo</strong>
+                    Alumnos de sexo Masculino:
+                    <strong><?php echo $datos['ninos'] ?></strong>
                   </h4>
                 </div>
                 <div class="col-sm-6">
                   <h4>
-                    Alumnos de sexo Femenino: <strong>algo</strong>
+                    Alumnos de sexo Femenino:
+                    <strong><?php echo $datos['ninas'] ?></strong>
                   </h4>
                 </div>
               </div>
               <!-- por peso min/max -->
               <div class="row">
+                <?php $query = "SELECT MAX(peso) as maximo,
+                  p_apellido,
+                  p_nombre
+                  from alumno
+                  inner join persona
+                  on alumno.cod_persona = persona.codigo
+                  where alumno.status = 1 and peso = (SELECT MAX(peso) from alumno);";
+                $resultado = conexion($query);
+                $datos = mysqli_fetch_assoc($resultado); ?>
                 <div class="col-sm-6">
                   <h4>
                     Mayor peso (kg):
-                    <strong>algo</strong>
-                    <a href="#">apellido, nombre</a>
+                    <strong><?php echo $datos['maximo'] ?></strong>
+                    <a href="#"><?php echo $datos['p_apellido'].", ".$datos['p_nombre'] ?></a>
                   </h4>
                 </div>
+                <?php $query = "SELECT MIN(peso) as minimo,
+                  p_apellido,
+                  p_nombre
+                  from alumno
+                  inner join persona
+                  on alumno.cod_persona = persona.codigo
+                  where alumno.status = 1 and peso = (SELECT MIN(peso) from alumno);";
+                $resultado = conexion($query);
+                $datos = mysqli_fetch_assoc($resultado); ?>
                 <div class="col-sm-6">
                   <h4>
                     Menor peso (kg):
-                    <strong>algo</strong>
-                    <a href="#">apellido, nombre</a>
+                    <strong><?php echo $datos['minimo'] ?></strong>
+                    <a href="#"><?php echo $datos['p_apellido'].", ".$datos['p_nombre'] ?></a>
                   </h4>
                 </div>
               </div>
               <!-- por peso -->
               <div class="row">
+                <?php $query = "SELECT round(AVG(peso), 2) as avg
+                  from alumno
+                  inner join persona
+                  on alumno.cod_persona = persona.codigo
+                  where alumno.status = 1 and persona.sexo = '0';";
+                $resultado = conexion($query);
+                $datos = mysqli_fetch_assoc($resultado); ?>
                 <div class="col-sm-6">
                   <h4>
-                    Media de peso en niños (kg): <strong>algo</strong>
+                    Media de peso en niños (kg):
+                    <strong><?php echo $datos['avg'] ?></strong>
                   </h4>
                 </div>
+                <?php $query = "SELECT round(AVG(peso), 2) as avg
+                  from alumno
+                  inner join persona
+                  on alumno.cod_persona = persona.codigo
+                  where alumno.status = 1 and persona.sexo = '1';";
+                $resultado = conexion($query);
+                $datos = mysqli_fetch_assoc($resultado); ?>
                 <div class="col-sm-6">
                   <h4>
-                    Media de peso en niñas (kg): <strong>algo</strong>
+                    Media de peso en niñas (kg):
+                    <strong><?php echo $datos['avg'] ?></strong>
                   </h4>
                 </div>
               </div>
               <!-- por altura min/max -->
               <div class="row">
+                <?php $query = "SELECT MAX(altura) as maximo,
+                  p_apellido,
+                  p_nombre
+                  from alumno
+                  inner join persona
+                  on alumno.cod_persona = persona.codigo
+                  where alumno.status = 1 and altura = (SELECT MAX(altura) from alumno);";
+                $resultado = conexion($query);
+                $datos = mysqli_fetch_assoc($resultado); ?>
                 <div class="col-sm-6">
                   <h4>
                     Mayor altura (cm):
-                    <strong>algo</strong>
-                    <a href="#">apellido, nombre</a>
+                    <strong><?php echo $datos['maximo'] ?></strong>
+                    <a href="#"><?php echo $datos['p_apellido'].", ".$datos['p_nombre'] ?></a>
                   </h4>
                 </div>
+                <?php $query = "SELECT MIN(altura) as minimo,
+                  p_apellido,
+                  p_nombre
+                  from alumno
+                  inner join persona
+                  on alumno.cod_persona = persona.codigo
+                  where alumno.status = 1 and altura = (SELECT MIN(altura) from alumno);";
+                $resultado = conexion($query);
+                $datos = mysqli_fetch_assoc($resultado); ?>
                 <div class="col-sm-6">
                   <h4>
                     Menor altura (cm):
-                    <strong>algo</strong>
-                    <a href="#">apellido, nombre</a>
+                    <strong><?php echo $datos['minimo'] ?></strong>
+                    <a href="#"><?php echo $datos['p_apellido'].", ".$datos['p_nombre'] ?></a>
                   </h4>
                 </div>
               </div>
               <!-- por altura -->
               <div class="row">
+                <?php $query = "SELECT round(AVG(peso), 2) as avg
+                  from alumno
+                  inner join persona
+                  on alumno.cod_persona = persona.codigo
+                  where alumno.status = 1 and persona.sexo = '0';";
+                $resultado = conexion($query);
+                $datos = mysqli_fetch_assoc($resultado); ?>
                 <div class="col-sm-6">
                   <h4>
-                    Media de altura en niños (cm): <strong>algo</strong>
+                    Media de altura en niños (cm):
+                    <strong><?php echo $datos['avg'] ?></strong>
                   </h4>
                 </div>
+                <?php $query = "SELECT round(AVG(peso), 2) as avg
+                  from alumno
+                  inner join persona
+                  on alumno.cod_persona = persona.codigo
+                  where alumno.status = 1 and persona.sexo = '1';";
+                $resultado = conexion($query);
+                $datos = mysqli_fetch_assoc($resultado); ?>
                 <div class="col-sm-6">
                   <h4>
-                    Media de altura en niñas (cm): <strong>algo</strong>
+                    Media de altura en niñas (cm):
+                    <strong><?php echo $datos['avg'] ?></strong>
                   </h4>
                 </div>
               </div>
               <!-- por repitiente -->
+              <?php $query = "SELECT
+                SUM(CASE WHEN repitiente = 's' THEN 1 ELSE 0 END) si,
+                SUM(CASE WHEN repitiente = 'n' THEN 1 ELSE 0 END) no,
+                COUNT(*) total
+                FROM alumno
+                where alumno.status = 1;";
+              $resultado = conexion($query);
+              $datos = mysqli_fetch_assoc($resultado); ?>
               <div class="row">
                 <div class="col-sm-6">
                   <h4>
-                    Alumnos repitientes: <strong>algo</strong>
+                    Alumnos repitientes:
+                    <strong><?php echo $datos['si'] ?></strong>
                   </h4>
                 </div>
                 <div class="col-sm-6">
                   <h4>
-                    Alumnos no repitientes: <strong>algo</strong>
+                    Alumnos no repitientes:
+                    <strong><?php echo $datos['no'] ?></strong>
                   </h4>
                 </div>
               </div>
