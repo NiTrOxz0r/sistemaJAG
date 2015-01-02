c03669f967d6315b4f3250c408fe1b93f7a2a283
slayerfat <slayerfat@gmail.com>
2015-01-02 15:07:06 -0430

    creados index.php para las carpetas

---
+++ personalAutorizado/reportes/index.php
@@ -0,0 +1,10 @@
+<?php
+if(!isset($_SESSION)){
+  session_start();
+}
+$enlace = $_SERVER['DOCUMENT_ROOT']."/github/sistemaJAG/php/master.php";
+require_once($enlace);
+validarUsuario(1);
+$index = enlaceDinamico();
+header("Location: $index");
+?>
