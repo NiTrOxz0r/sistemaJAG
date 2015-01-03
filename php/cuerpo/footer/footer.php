3693cd8a79fe751388c7b21c74c79c92fac36fb0
slayerfat <slayerfat@gmail.com>
2015-01-03 13:47:53 -0430

    añadidos detalles adicionales: favicon. logo. mas ccs.

--- php/cuerpo/footer/footer.php
+++ php/cuerpo/footer/footer.php
@@ -32,14 +32,48 @@ $bootstrapJS = enlaceDinamico('css/bootstrap/js/bootstrap.min.js');
         </ul>
       </div>
       <div class="col-sm-2">
-        <div id="footerLogo"></div>
+        <div id="footerLogo">
+          <?php $enlace = enlaceDinamico('imagenes/logo-escuela-edge.png'); ?>
+          <?php $index = enlaceDinamico(); ?>
+          <center>
+            <a href="<?php echo $index ?>">
+              <img src="<?php echo $enlace ?>" class="img-responsive" width="50px" alt="sistema de inscripcion jag">
+            </a>
+          </center>
+        </div>
       </div>
-      <div class="col-sm-4">
-        <ul class="text-right">
-          <li>algo</li>
-          <li>algo</li>
-          <li>algo</li>
-          <li>algo</li>
+      <div class="col-sm-4 text-right">
+        <div>
+          <h4 style="padding-left:40px;">
+            <?php if ($_SESSION['cod_tipo_usr'] >= 1 and $_SESSION['cod_tipo_usr'] <> 5): ?>
+              <strong>Gestionar</strong>
+            <?php else: ?>
+              <strong>sistemaJAG</strong>
+            <?php endif ?>
+          </h4>
+        </div>
+        <ul>
+          <strong>
+          <?php if ($_SESSION['cod_tipo_usr'] >= 1 and $_SESSION['cod_tipo_usr'] <> 5): ?>
+            <?php $alumnos = enlaceDinamico('alumno/menucon.php'); ?>
+            <?php $padres = enlaceDinamico('personalAutorizado/menucon.php'); ?>
+            <?php $cursos = enlaceDinamico('curso/menucon.php'); ?>
+            <?php $usuarios = enlaceDinamico('usuario/menucon.php'); ?>
+            <li><a href="<?php echo $alumnos ?>">Alumno</a></li>
+            <li><a href="<?php echo $padres ?>">Padres/Allegados</a></li>
+            <li><a href="<?php echo $cursos ?>">Cursos</a></li>
+            <?php if ($_SESSION['cod_tipo_usr'] >= 2): ?>
+              <li><a href="<?php echo $usuarios ?>">Usuarios</a></li>
+            <?php endif ?>
+          <?php else: ?>
+            <?php $enlace = enlaceDinamico('sobre.php'); ?>
+            <li><a href="<?php echo $enlace ?>">Sobre el sistemaJAG</a></li>
+            <?php $enlace = enlaceDinamico('manual.php'); ?>
+            <li><a href="#">Manual de uso</a></li>
+            <?php $enlace = enlaceDinamico('manual.php'); ?>
+            <li><a href="#">Ayuda al usuario</a></li>
+          <?php endif ?>
+          </strong>
         </ul>
       </div>
     </div>
@@ -71,13 +105,13 @@ $bootstrapJS = enlaceDinamico('css/bootstrap/js/bootstrap.min.js');
     </div>
   </div>
   <p id="mainTitulo">
-    sistemaJAG 2014.
+    <strong>sistemaJAG 2014.</strong>
   </p>
   <script type="text/javascript">
-  <?php $enlace = 'imagenes/'.rand(1,15).'.png';
-  $imagen = enlaceDinamico($enlace) ?>
-  $(function() {
-    $('#imgCodigos').css('background-image','url(<?php echo $imagen ?>)');
-  });
+    <?php $enlace = 'imagenes/'.rand(1,15).'.png';
+    $imagen = enlaceDinamico($enlace) ?>
+    $(function() {
+      $('#imgCodigos').css('background-image','url(<?php echo $imagen ?>)');
+    });
   </script>
 </footer>
